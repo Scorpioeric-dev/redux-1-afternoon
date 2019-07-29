d2524a1cf84fec78995edd20fee20e5459b45199
Scorpioeric-dev <ericscorpio1@gmail.com>
2019-07-29 16:57:50 -0600

    redux-afternoon-1

--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import store, {ADD_INGREDIENT} from "./../../store"
 
 class Instructions extends Component {
   constructor(props) {
     super(props);
+    const reduxState = store.getState()
+
     this.state = {
-      instructions: [],
+      instructions: reduxState.instructions,
       input: ""
     };
   }
+
+componentDidMount(){
+  store.subscribe(() => {
+    const reduxState = store.getState()
+    this.setState({
+      instructions:reduxState.instructions
+    })
+  })
+}
+
+
+
+
+
+
   handleChange(val) {
     this.setState({
       input: val
--- src/store.js
+++ src/store.js
@@ -1,1 +1,58 @@
+import { createStore } from "redux";
+//importing createstore from redux library
 
+//setting init state to empty obj
+const initialState = {
+  name: "",
+  category: "",
+  authorFirst: "",
+  authorLast: "",
+  ingredients: [],
+  instructions:[],
+  recipes: []
+};
+
+
+
+export const ADD_RECIPE = 'ADD_RECIPE'
+export const ADD_INSTRUCTION = "ADD_INSTRUCTION"
+export const ADD_INGREDIENT = "ADD_INGREDIENTS";
+export const UPDATE_NAME = "UPDATE_NAME";
+export const UPDATE_CATEGORY = "UPDATE_CATEGORY";
+export const UPDATE_AUTHOR_FIRST = "UPDATE_AUTHOR_FIRST";
+export const UPDATE_AUTHOR_LAST = "UPDATE_AUTHOR_FIRST";
+//creating reducer function ? Study this more (short of it the motor for redux with its particular method type)
+function reducer(state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case UPDATE_NAME:
+      return { ...state, name: payload };
+
+    case UPDATE_CATEGORY:
+      return { ...state, category: payload };
+
+    case UPDATE_AUTHOR_FIRST:
+      return { ...state, authorFirst: payload };
+
+    case UPDATE_AUTHOR_LAST:
+      return { ...state, authorLast: payload };
+
+    case ADD_INGREDIENT:
+      const newIngredients = [...state.ingredients, payload]
+      return { ...state, ingredients: newIngredients };
+
+      case ADD_INSTRUCTION:
+        const newInstructions = [...state.instructions,payload]
+        return{...state,instructions:newInstructions}
+
+        case ADD_RECIPE:
+          const newRecipes = [...state.recipes,payload]
+          return{...state,recipes:newRecipes}
+
+    default:
+      return state;
+  }
+}
+
+export default createStore(reducer);
